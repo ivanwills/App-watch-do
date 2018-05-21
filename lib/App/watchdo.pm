18ac449c7a4165bd1a09bbd397394b14c40aedc1
Ivan Wills <Ivan.Wills2@optus.com.au>
2018-05-21 11:24:19 +1000

    Updating for release of version 0.1.0

--- lib/App/watchdo.pm
+++ lib/App/watchdo.pm
@@ -18,7 +18,7 @@ use AnyEvent::Loop;
 use AnyEvent::Filesys::Notify;
 use Path::Tiny;
 
-our $VERSION = version->new('0.0.7');
+our $VERSION = version->new('0.1.0');
 
 has [qw/git run done/] => ( is => 'rw' );
 has [qw/dirs files exclude changed/] => (
@@ -144,7 +144,7 @@ App::watchdo - Run a command when watched files change
 
 =head1 VERSION
 
-This documentation refers to App::watchdo version 0.0.7
+This documentation refers to App::watchdo version 0.1.0
 
 =head1 SYNOPSIS
 
